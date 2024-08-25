cba0019a5f2698adfbfe751567541eb981d5e75b
patman <27874300+patmanf@users.noreply.github.com>
2024-08-25 16:59:34 -0500

    make it less shit

--- quickbar/pat_duplicate/me.lua
+++ quickbar/pat_duplicate/me.lua
@@ -1,58 +1,46 @@
-do
-	local c = root.assetJson("/quickbar/icons.json")
-	local i = c.items["patman:duplicate"]
+local icon = root.assetJson("/quickbar/icons.json:items.patman:duplicate")
 
-	--old quickbar
-	if not metagui then
-		local function action(_, script, ...)
-			if type(script) ~= "string" then return nil end
-			params = {...}
-			_SBLOADED[script] = nil require(script)
-			params = nil
-		end
+local function addIcon()
+	pane.playSound(icon.sound)
 
-		local l = "scroll.list." .. widget.addListItem("scroll.list")
-		widget.setText(l .. ".label", i.label)
-		local bc = l .. ".buttonContainer"
-
-		widget.registerMemberCallback(bc, "click", function()
-			action(table.unpack(i.action))
-			if i.dismissQuickbar then pane.dismiss() end
-		end)
-		local btn = bc .. "." .. widget.addListItem(bc) .. ".button"
-		widget.setButtonOverlayImage(btn, i.icon or "/items/currency/essence.png")
-
-		if config.getParameter("compacted") and compactLabels then
-			compactLabels[btn] = i.label
-		end
-
-	--new quickbar
-	else
-		local function action(id, ...) return (qbActions[id] or function() end)(...) end
-		local function menuClick(w, btn)
-			local i = w.data
-			if i.condition and not condition(table.unpack(i.condition)) then return nil end
-			action(table.unpack(i.action))
-		end
-		
-		local width = 128
-		local itmHeight = 20
-		
-		local cum = itemField:addChild({
-			type = "menuItem", size = {width, itmHeight}, data = i, padding = 0,
-			children = { { scissoring = false },
-				{ type = "label", align = "right", text = i.label },
-				{ type = "image", size = {itmHeight, itmHeight}, file = i.icon or "/items/currency/essence.png" },
-			}
-		})
-    
-    metagui.startEvent(function()
-      util.wait(0.05)
-      itemField:scrollBy({0, 20})
-    end)
-		
-		cum.onClick = menuClick
+	-- Classic Quickbar
+	if addQuickbarButton then
+		addQuickbarButton(icon)
+		return
 	end
 
-	pane.playSound("/quickbar/pat_duplicate/me.ogg")
+	-- Stardust Core
+	if metagui then
+		local newItem = itemField:addChild({
+			type = "menuItem", size = {128, 20}, data = icon, padding = 0,
+			children = { { scissoring = false },
+				{ type = "label", align = "right", text = icon.label },
+				{ type = "image", size = {20, 20}, file = icon.icon },
+			}
+		})
+		newItem.onClick = addIcon
+
+		metagui.startEvent(function()
+			coroutine.yield()
+			itemField:scrollBy({0, 20})
+		end)
+
+		return
+	end
+
+	-- Quickbar Mini
+	local listItem = string.format("scroll.list.%s", widget.addListItem("scroll.list"))
+	widget.setText(listItem .. ".label", icon.label)
+
+	local container = listItem .. ".buttonContainer"
+	widget.registerMemberCallback(container, "click", addIcon)
+
+	local button = string.format("%s.%s.button", container, widget.addListItem(container))
+	widget.setButtonOverlayImage(button, icon.icon)
+
+	if compactLabels then
+		compactLabels[button] = icon.label
+	end
 end
+
+addIcon()
